62026551049f727b5e8f9f1fc54b25f74304f98f
xiangshouding <fansekey@gmail.com>
2026-05-19 11:47:20 +0800

    Update morph formula to v0.2.0

--- Formula/morph.rb
+++ Formula/morph.rb
@@ -1,9 +1,9 @@
 class Morph < Formula
   desc "Terminal-native multimodal AI agent written in pure C"
   homepage "https://github.com/oxUnd/morph"
-  url "https://github.com/oxUnd/morph/archive/1191628db7c1f4bb627c0cfa2465281765c3e0ef.tar.gz"
-  sha256 "4930476a16b41fa3aa9989617d99f3a94ae7fe1c3665efb6a0e0444bebea9369"
-  version "0.1.0"
+  url "https://github.com/oxUnd/morph/archive/6d0e44a10c4002ea118f8fa109e94127b58d49d9.tar.gz"
+  sha256 "ab007c7f3f6d0b525ecee6788992242ec8a3eca431e18f0075012568a951fa60"
+  version "0.2.0"
   license "all-rights-reserved"
 
   depends_on "cmake" => :build
