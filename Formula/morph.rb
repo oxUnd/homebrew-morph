3a07ffd5cdb463d4b1a90bbb267df8cfc97a566f
oxUnd <fansekey@gmail.com>
2026-05-31 13:46:03 +0800

    Update morph formula to v0.3.3

--- Formula/morph.rb
+++ Formula/morph.rb
@@ -1,23 +1,24 @@
 class Morph < Formula
   desc "Terminal-native multimodal AI agent written in pure C"
   homepage "https://github.com/oxUnd/morph"
-  url "https://github.com/oxUnd/morph/archive/54228760bda8826e8bede13456714a29a6e77107.tar.gz"
-  sha256 "7a1f3db7f696d7dcc709865102919cb8bcd8411e640468203a1957403437fd84"
-  version "0.3.2"
+  url "https://github.com/oxUnd/morph/archive/c64283f81f17f813c0de6cb6b683f65a745f6766.tar.gz"
+  version "0.3.3"
+  sha256 "67b3e1cda3251b978639365bbd247361ed428324d59948728eff01c2b9833169"
   license "all-rights-reserved"
 
   depends_on "cmake" => :build
-  depends_on "md4c"
-  depends_on "sqlite"
   depends_on "curl"
   depends_on "libuv"
+  depends_on "md4c"
   depends_on "morph-editor"
+  depends_on "sqlite"
   depends_on "readline" => :recommended
 
   def install
     inreplace "CMakeLists.txt" do |s|
-      s.gsub! /include\(FetchContent\)/, ""
-      s.gsub! /FetchContent_Declare\(\n\tmd4c\n\tURL      https:\/\/github\.com\/mity\/md4c\/archive\/refs\/tags\/release-0\.5\.3\.tar\.gz\n\)\nFetchContent_MakeAvailable\(md4c\)/, "find_package(md4c REQUIRED)"
+      s.gsub! "include(FetchContent)", ""
+      s.gsub! %r{FetchContent_Declare\(\n\tmd4c\n\tURL      https://github\.com/mity/md4c/archive/refs/tags/release-0\.5\.3\.tar\.gz\n\)\nFetchContent_MakeAvailable\(md4c\)},
+              "find_package(md4c REQUIRED)"
     end
 
     inreplace "src/render/CMakeLists.txt", "md4c", "md4c::md4c"
@@ -30,6 +31,16 @@ class Morph < Formula
     bin.install "build/morph"
 
     (etc/"morph").install "config.toml.example" => "config.toml.example"
+    (pkgshare/"tiktoken").install Dir["vendor/tiktoken/*.tiktoken"]
+  end
+
+  def post_install
+    tiktoken_dir = Pathname.new(Dir.home) / ".morph" / "tiktoken"
+    tiktoken_dir.mkpath
+    %w[cl100k_base.tiktoken o200k_base.tiktoken].each do |f|
+      src = pkgshare / "tiktoken" / f
+      cp src, tiktoken_dir / f if src.exist?
+    end
   end
 
   test do
